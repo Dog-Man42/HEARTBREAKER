2effe03ad44ad0199269ee4a8c4fe075082b0cf7
Tucker Agee <tuckturtle15@gmail.com>
2023-10-23 22:31:08 -0400

    Added to repository

---
+++ src/Heartbreaker/objects/TentacleSegment.java
@@ -0,0 +1,42 @@
+package Heartbreaker.objects;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+public class TentacleSegment extends BaseObject{
+
+    private int radius;
+
+    private double baseX;
+    private double baseY;
+
+
+
+    public TentacleSegment(int radius, double scale, double baseX, double baseY){
+        this.scale = scale;
+        this.radius = radius;
+        this.baseX = baseX;
+        this.baseY = baseY;
+        xPosition = baseX;
+        yPosition = baseY;
+    }
+
+    public void setPosition(Point2D.Double p){
+        xPosition = p.x;
+        yPosition = p.y;
+    }
+
+
+
+
+    @Override
+    public void update() {}
+
+    public void draw(Graphics2D g){
+        g.setColor(Color.ORANGE);
+        g.fillOval((int) (getxPosition(true) - radius), (int) (getyPosition(true) - radius), radius*2,radius*2);
+
+
+
+    }
+}
